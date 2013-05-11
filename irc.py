6640be7e5bd4700f8020fb2232fd7633f0c1f1c0
Liam Stanley <me@liamstanley.net>
2013-05-10 22:34:19 -0400

    re-add spam notify with ...

--- irc.py
+++ irc.py
@@ -345,7 +345,7 @@ class Bot(asynchat.async_chat):
             messages = [m[1] for m in self.stack[-8:]]
             if messages.count(text) >= 5:
                 text = '...'
-                if messages.count('...') > 1:
+                if messages.count('...') > 2:
                     self.sending.release()
                     return
 
