67b2df2bf91c901953c4bc9511955384dad75c47
Liam Stanley <me@liamstanley.net>
2013-05-11 15:35:56 -0400

    reverse list, steal first alphanumric string

--- modules/calc.py
+++ modules/calc.py
@@ -88,9 +88,9 @@ def calc(code, input):
       answer = web.decode(answer)
       if any(c.isalpha() for c in answer):
           #re.split('(\d+)',answer)
-          #answer = answer[::-1]
+          arev = answer[::-1]
           alist = answer.split()
-          endword = alist[len(alist-1)]
+          endword = arev[0]
           number = answer.replace(endword, '')
           number = number.strip(' ')
           code.say(code.bold(number) + ' ' + code.color('blue', endword))
