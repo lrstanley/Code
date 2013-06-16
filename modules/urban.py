e352547cdfcc4c2bb47cea4a479fa6beeeab6b66
Liam Stanley <me@liamstanley.net>
2013-06-15 23:10:11 -0400

    added random urban definitions via .urban

--- modules/urban.py
+++ modules/urban.py
@@ -13,29 +13,40 @@ h = HTMLParser.HTMLParser()
 
 # My present to you.
 def urban(code, input):
-    if not input.group(2): return code.say(code.color('red', 'Please enter an input.'))
     # clean and split the input
-    msg = input.group(2).lower().strip()
-    parts = msg.split()
-    if parts[-1].replace('-','').isdigit():
-        if int(parts[-1]) <= 0:
-            id = 1
+    if input.group(2):
+        msg = input.group(2).lower().strip()
+        parts = msg.split()
+        if parts[-1].replace('-','').isdigit():
+            if int(parts[-1]) <= 0:
+                id = 1
+            else:
+                id = int(parts[-1].replace('-',''))
+            del parts[-1]
+            query = '+'.join(parts)
         else:
-            id = int(parts[-1].replace('-',''))
-        del parts[-1]
-        query = '+'.join(parts)
+            id = 1
+            query = msg.replace(' ', '+')
+        uri = 'http://www.urbandictionary.com/define.php?term=%s'
+        query = re.sub(r'[^\w\s]', '+', query)
+        query = query.replace('.', '+')
+        while query.find('++') > -1:
+            query = query.replace('++', '+').strip('+')
+        try:
+            r = urllib2.urlopen(uri % (query)).read().replace('\t','').replace('\r',' ').replace('\n','').decode('utf-8')
+        except urllib2.HTTPError as e:
+            return code.say(code.color('red', 'urbandictionary.com did not respond correctly, is it down?'))
+        definitive = 'Definition'
     else:
         id = 1
-        query = msg.replace(' ', '+')
-    uri = 'http://www.urbandictionary.com/define.php?term=%s'
-    query = re.sub(r'[^\w\s]', '+', query)
-    query = query.replace('.', '+')
-    while query.find('++') > -1:
-        query = query.replace('++', '+').strip('+')
-    try:
-        r = urllib2.urlopen(uri % (query)).read().replace('\t','').replace('\r','').replace('\n',' ').decode('utf-8')
-    except urllib2.HTTPError as e:
-        return code.say(code.color('red', 'urbandictionary.com did not respond correctly, is it down?'))
+        uri = 'http://www.urbandictionary.com/random.php'
+        try:
+            r = urllib2.urlopen(uri).read().replace('\t','').replace('\r','').replace('\n',' ').decode('utf-8')
+        except urllib2.HTTPError as e:
+            return code.say(code.color('red', 'urbandictionary.com did not respond correctly, is it down?'))
+        name = re.compile(r'<title>.*?</title>').findall(r)[0].replace('Urban Dictionary: ','')
+        name = re.sub(r'\<.*?\>', '', name).strip()
+        definitive = 'Definition (%s)' % name
     definition = re.compile(r'<div class="definition">.*?</div>').findall(r)
     example = re.compile(r'<div class="example">.*?</div>').findall(r)
     did = len(definition)
@@ -49,7 +60,7 @@ def urban(code, input):
         code.say('(%s/%s) %s: %s' % (str(id), str(did), code.color('purple', 'Definition'), \
                  h.unescape(definition)))
     else:
-        code.say('(%s/%s) %s: %s %s: %s' % (str(id), str(did), code.color('purple', 'Definition'), \
+        code.say('(%s/%s) %s: %s %s: %s' % (str(id), str(did), code.color('purple', definitive), \
                  h.unescape(definition), code.color('purple', 'Ex'), h.unescape(example)))
 urban.commands = ['urban', 'ur']
 urban.example = '.urban liam'
