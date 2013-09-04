3dc4c0c089f6bc115db2e639f2c1763e23d43752
Liam Stanley <me@liamstanley.net>
2013-09-04 01:25:14 -0400

    modify quote.py, to use local quotes, where the owner and add/delete quote

--- modules/quote.py
+++ modules/quote.py
@@ -6,21 +6,88 @@ quote.py - Code Quote Module
 http://code.liamstanley.net/
 """
 
-import re
-import web
-
-quoteuri = 'http://www.leonatkinson.com/random/index.php/rest.html?method=advice'
-r_paragraph = re.compile(r'<quote>.*?</quote>')
+import random
+from modules import unicode as uc
 
 
-def getquote(code, input):
-    page = web.get(quoteuri)
-    paragraphs = r_paragraph.findall(page)
-    line = re.sub(r'<[^>]*?>', '', unicode(paragraphs[0]))
-    code.say(code.bold(line.lower().capitalize() + "."))
-getquote.commands = ['quote']
-getquote.thread = False
-getquote.rate = 30
+def addquote(code, input):
+    '''.addquote <nick> something they said here -- adds the quote to the quote database.'''
+    text = input.group(2)
+    if not text:
+        return code.say('No quote provided')
+    fn = open('quotes.txt', 'a')
+    output = uc.encode(text)
+    fn.write(output)
+    fn.write('\n')
+    fn.close()
+    code.reply('Quote added.')
+addquote.commands = ['addquote']
+addquote.priority = 'low'
+addquote.example = '.addquote'
+addquote.rate = 30
+
+
+def retrievequote(code, input):
+    '''.quote <number> -- displays a given quote'''
+    text = input.group(2)
+    try:
+        fn = open('quotes.txt', 'r')
+    except:
+        return code.reply('Please add a quote first.')
+
+    lines = fn.readlines()
+    MAX = len(lines)
+    fn.close()
+    random.seed()
+    try:
+        number = int(text)
+        if number < 0:
+            number = MAX - abs(number) + 1
+    except:
+        number = random.randint(1, MAX)
+    if not (0 <= number <= MAX):
+        code.reply("I'm not sure which quote you would like to see.")
+    else:
+        line = lines[number - 1]
+        code.reply('Quote %s of %s: ' % (number, MAX) + line)
+retrievequote.commands = ['quote']
+retrievequote.priority = 'low'
+retrievequote.example = '.quote'
+retrievequote.rate = 30
+
+
+def delquote(code, input):
+    '''.rmquote <number> -- removes a given quote from the database. Can only be done by the owner of the bot.'''
+    if not input.owner: return
+    text = input.group(2)
+    number = int()
+    try:
+        fn = open('quotes.txt', 'r')
+    except:
+        return code.reply('No quotes to delete.')
+    lines = fn.readlines()
+    MAX = len(lines)
+    fn.close()
+    try:
+        number = int(text)
+    except:
+        code.reply('Please enter the quote number you would like to delete.')
+        return
+    newlines = lines[:number-1] + lines[number:]
+    fn = open('quotes.txt', 'w')
+    for line in newlines:
+        txt = uc.encode(line)
+        if txt:
+            fn.write(txt)
+            if txt[-1] != '\n':
+                fn.write('\n')
+    fn.close()
+    code.reply('Successfully deleted quote %s.' % (number))
+delquote.commands = ['rmquote', 'delquote','deletequote','removequote']
+delquote.priority = 'low'
+delquote.example = '.rmquote'
+delquote.rate = 30
+
 
 if __name__ == '__main__':
     print __doc__.strip()
