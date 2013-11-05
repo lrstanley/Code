2c8127f2904a47d2ac8200434dca92176ddb70b5
Liam Stanley <me@liamstanley.net>
2013-11-05 01:21:07 -0500

    Rework .rand to be a lot simpler, as well as fix a possible crash.

--- modules/ask.py
+++ modules/ask.py
@@ -33,52 +33,58 @@ ask.example = '.ask today or tomorrow or next week'
 
 def rand(code, input):
     """.rand <arg1> <arg2> - Generates a random integer between <arg1> and <arg2>."""
-    if input.group(2) == " " or not input.group(2):
-        code.say("I'm sorry, " + str(input.nick) + ', but you must enter at least one number.')
-    else:
-        random.seed()
-        li_integers = input.group(2)
-        li_integers_str = li_integers.split()
-        if len(li_integers_str) == 1:
-            li_integers_str = re.sub(r'\D', '', str(li_integers_str))
-            if len(li_integers_str) > 0:
-                if int(li_integers_str[0]) <= 1:
-                    a = li_integers_str
-                    a = int(a)
-                    if a < 0:
-                        randinte = random.randint(a, 0)
-                    if a > 0:
-                        randinte = random.randint(0, a)
-                else:
-                    a = li_integers_str
-                    a = int(a)
-                    randinte = random.randint(0, a)
-                code.say(str(input.nick) + code.color('lime', ': your random integer is: ') + str(randinte))
-            else:
-                code.reply(code.bold('lolwut'))
-        else:
-            ln = li_integers.split()
-            if len(ln) == 2:
-                a, b = ln
-                a = re.sub(r'\D', u'', a)
-                b = re.sub(r'\D', u'', b)
-                if not a:
-                    a = 0
-                if not b:
-                    b = 0
-                a = int(a)
-                b = int(b)
-                if a <= b:
-                    randinte = random.randint(a, b)
-                else:
-                    randinte = random.randint(b, a)
-                code.say(str(input.nick) + ': your random integer is: ' + str(randinte))
-            else:
-                code.reply(code.color('red', 'I\'m not sure what you want me to do!'))
+    # random.randint(a, 0)
+    syntax = 'Syntax: \'.rand <number> <number>\''
+    toolong = 'Woah man! That\'s really long!'
+    if not input.group(2):
+        return code.reply('Syntax: \'.rand <number> <number>\'')
 
+    msg = input.group(2)
+    
+    try:
+        if ',' in msg:
+            # Assume the user is doing .rand #,#
+            first,second = msg.strip().split(',',1)
+            first = int(first)
+            second = int(second)
+            if too_long(first) or too_long(second):
+                return code.reply(toolong)
+            if first > second:
+                small = second
+                big = first
+            else:
+                small = first
+                big = second
+
+        elif ' ' in msg and len(msg.split()) == 2:
+            # Assume the user is doing .rand # #
+            first,second = msg.strip().split()
+            first = int(first)
+            second = int(second)
+            if too_long(first) or too_long(second):
+                return code.reply(toolong)
+            if first > second:
+                small = second
+                big = first
+            else:
+                small = first
+                big = second
+        else:
+            return code.reply(syntax)
+
+        # Now respond
+        number = str(random.randint(small,big))
+        return code.reply('Your random integer is: %s' % code.color('red',number))
+    except:
+        return code.reply(syntax)
 rand.commands = ['rand']
 rand.priority = 'medium'
 
+def too_long(integer):
+    if len(str(integer)) > 10:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     print __doc__.strip()
