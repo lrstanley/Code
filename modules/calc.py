59009b1d06860e84dbfde501444671f73187f169
Liam Stanley <me@liamstanley.net>
2013-11-07 09:12:32 -0500

    Update calc.py

--- modules/calc.py
+++ modules/calc.py
@@ -14,97 +14,17 @@ import unicodedata
 r_result = re.compile(r'(?i)<A NAME=results>(.*?)</A>')
 r_tag = re.compile(r'<\S+.*?>')
 
-subs = [
-    (' in ', ' -> '), 
-    (' over ', ' / '), 
-    (u'£', 'GBP '), 
-    (u'€', 'EUR '), 
-    ('\$', 'USD '), 
-    (r'\bKB\b', 'kilobytes'), 
-    (r'\bMB\b', 'megabytes'), 
-    (r'\bGB\b', 'kilobytes'), 
-    ('kbps', '(kilobits / second)'), 
-    ('mbps', '(megabits / second)')
-]
-
-def dcalc(code, input): 
-    """Frink Online Calculator"""
-    q = input.group(2)
-    if not q: 
-        return code.say('0?')
-
-    query = q[:]
-    for a, b in subs: 
-        query = re.sub(a, b, query)
-    query = query.rstrip(' \t')
-
-    precision = 5
-    if query[-3:] in ('GBP', 'USD', 'EUR', 'NOK'): 
-        precision = 2
-    query = web.urllib.quote(query.encode('utf-8'))
-
-    uri = 'http://futureboy.us/fsp/frink.fsp?fromVal='
-    bytes = web.get(uri + query)
-    m = r_result.search(bytes)
-    if m: 
-        result = m.group(1)
-        result = r_tag.sub('', result) # strip span.warning tags
-        result = result.replace('&gt;', '>')
-        result = result.replace('(undefined symbol)', '(?) ')
-
-        if '.' in result: 
-            try: result = str(round(float(result), precision))
-            except ValueError: pass
-
-        if not result.strip(): 
-            result = '?'
-        elif ' in ' in q: 
-            result += ' ' + q.split(' in ', 1)[1]
-
-        code.say(q + ' = ' + result[:350])
-    else: code.reply(code.color('red', 'Sorry, can\'t calculate that.'))
-    code.say(code.color('blue', 'Note that .dcalc/.cd is deprecated, consider using .calc'))
-dcalc.commands = ['dcalc', 'dc']
-dcalc.example = '.dcalc 5 + 3'
-
-def calc(code, input): 
-    """Google calculator."""
-    if not input.group(2): return code.reply(code.color('red', 'Nothing to calculate.'))
-    q = input.group(2)
-    if '=' in q and not q.endswith('='):
-        return code.reply(code.color('red', 'I cannot solve equations.'))
-    q = q.replace('x','*').replace('=','').strip().encode('utf-8')
-    # utf-8 U+03D5, utf-8 U+03C0
-    q = q.replace('\xcf\x95', 'phi').replace('\xcf\x80', 'pi')
-    uri = 'http://www.google.com/ig/calculator?q='
-    bytes = web.get(uri + web.urllib.quote(q))
-    parts = bytes.split('",')
-    answer = [p for p in parts if p.startswith('rhs: "')][0][6:]
-    if answer: 
-        answer = answer.decode('unicode-escape')
-        answer = ''.join(chr(ord(c)) for c in answer)
-        answer = answer.decode('utf-8')
-        answer = answer.replace(u'\xc2\xa0', ',')
-        answer = answer.replace('<sup>', '^(')
-        answer = answer.replace('</sup>', ')')
-        answer = web.decode(answer)
-        answer.encode('ascii','ignore')
-        #if any(c.isalpha() for c in answer):
-        try:
-             #firstchar = re.search(r'(\d )+ (.*)',answer)
-             fletter = next(i for i, c in enumerate(answer) if c.isalpha())
-             number = answer[0:fletter-1]
-             words = answer[fletter:len(answer)]
-             number.strip()
-             cnumb = number.join(number.split())
-             words = ' ' + words.strip()
-             #[17:02:55] <Avaris> Liam-: next(i for i, c in enumerate(s) if c.isalpha())
-             #[17:03:59] <Avaris> (throws StopIteration if there is no letter in the original string)
-             code.say(code.bold(cnumb) + code.color('blue', words))
-        except:
-             answer.strip()
-             code.say(answer)
-    else: code.say(code.color('red', 'Sorry, no result.'))
+def calc(code, input):
+    if not input.group(2): return code.reply('Failed to calculate!')
+    try:
+        data = json.loads(urllib2.urlopen(uri % urllib.quote(input.group(2))).read())
+        if data['AnswerType'] != 'calc':
+            return code.reply('Failed to calculate!')
+        #print data['Answer']
+        answer = re.sub(r'\<.*?\>', '', data['Answer']).strip()
+        print answer
+    except:
+        return code.reply('Failed to calculate!')
 calc.commands = ['c', 'calc', 'calculate']
 calc.example = '.calc 5 + 3'
 
