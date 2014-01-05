2fcdf758f817658c2483a8f9ac570b34ea5addcb
Liam Stanley <me@liamstanley.net>
2014-01-05 14:30:17 -0500

    Fix no-arg bug in .countdown

--- modules/countdown.py
+++ modules/countdown.py
@@ -8,17 +8,17 @@ http://code.liamstanley.net/
 
 import datetime
 
-def generic_countdown(code, input):
+def countdown(code, input):
     """ .countdown <year> <month> <day> - displays a countdown to a given date. """
-    try:
-        text = input.group(2).split()
-    except:
-        code.say(code.color('red', 'Please use correct format: .countdown <year> <month> <day>'))
+    error = 'Please use correct format: .countdown <year> <month> <day>'
+    if not input.group(2):
+        return code.say(code.color('red', error))
+    text = input.group(2).split()
     if text[0].isdigit() and text[1].isdigit() and text[2].isdigit() and len(text) == 3:
         diff = datetime.datetime(int(text[0]), int(text[1]), int(text[2])) - datetime.datetime.today()
         code.say(str(diff.days) + "-days " +  str(diff.seconds/60/60) + "-hours " +  str(diff.seconds/60 - diff.seconds/60/60 * 60) + "-minutes until " + text[0] + " " + text[1] + " " + text[2])
     else:
-        code.say(code.color('red', 'Please use correct format: .countdown <year> <month> <day>'))
+        code.say(code.color('red', error))
 generic_countdown.commands = ['countdown']
 generic_countdown.priority = 'low'
 
