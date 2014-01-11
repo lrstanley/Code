94fae7f13df68718a6df58a51960b2925a033c39
Liam Stanley <me@liamstanley.net>
2014-01-11 13:31:06 -0500

    Update wikipedia.py

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -1,182 +1,105 @@
 #!/usr/bin/env python
 """
 Code Copyright (C) 2012-2013 Liam Stanley
-Credits: Sean B. Palmer, Michael Yanovich
 wikipedia.py - Code Wikipedia Module
 http://code.liamstanley.net/
 """
 
-import re, urllib, gzip, StringIO
-import web
+from urllib import quote as urlify
+from urllib2 import urlopen as get
+from json import loads as jsonify
 
-wikiuri = 'http://%s.wikipedia.org/wiki/%s'
-# wikisearch = 'http://%s.wikipedia.org/wiki/Special:Search?' \
-#                     + 'search=%s&fulltext=Search'
+lang = 'en'
+maxlen = '220'
 
-r_tr = re.compile(r'(?ims)<tr[^>]*>.*?</tr>')
-r_paragraph = re.compile(r'(?ims)<p[^>]*>.*?</p>|<li(?!n)[^>]*>.*?</li>')
-r_tag = re.compile(r'<(?!!)[^>]+>')
-r_whitespace = re.compile(r'[\t\r\n ]+')
-r_redirect = re.compile(
-   r'(?ims)class=.redirectText.>\s*<a\s*href=./wiki/([^"/]+)'
-)
+wikipedia_url = 'wikipedia.org'
+wikiquote_url = 'wikiquote.org'
 
-abbrs = ['etc', 'ca', 'cf', 'Co', 'Ltd', 'Inc', 'Mt', 'Mr', 'Mrs', 
-         'Dr', 'Ms', 'Rev', 'Fr', 'St', 'Sgt', 'pron', 'approx', 'lit', 
-         'syn', 'transl', 'sess', 'fl', 'Op', 'Dec', 'Brig', 'Gen'] \
-   + list('ABCDEFGHIJKLMNOPQRSTUVWXYZ') \
-   + list('abcdefghijklmnopqrstuvwxyz')
-t_sentence = r'^.{5,}?(?<!\b%s)(?:\.(?=[\[ ][A-Z0-9]|\Z)|\Z)'
-r_sentence = re.compile(t_sentence % r')(?<!\b'.join(abbrs))
+full_define = 'https://%s.%s/w/api.php?action=query&prop=extracts&explaintext&exsectionformat=plain&exchars=%s&titles=%s&format=json'
+full_search = 'https://%s.%s/w/api.php?action=opensearch&search=%s&format=json'
 
-def unescape(s): 
-   s = s.replace('&gt;', '>')
-   s = s.replace('&lt;', '<')
-   s = s.replace('&amp;', '&')
-   s = s.replace('&#160;', ' ')
-   return s
 
-def text(html): 
-   html = r_tag.sub('', html)
-   html = r_whitespace.sub(' ', html)
-   return unescape(html).strip()
+def wikiSearch(query, url, results=5):
+    """Use MediaWikis API to search for values from wiktionary and wikipedia"""
+    # First, we need to grab the data, and serialize it in JSON
+    url_query = urlify(query)
+    data = jsonify(get(full_search % (lang, url, url_query)).read())
 
-def search(term): 
-   try: import search
-   except ImportError, e: 
-      print e
-      return term
+    # Check to see if we have #(results as arg form) results, then make a list
+    if not data[1]:
+        return False
+    if len(data[1]) > results:
+        return data[1][:results]
+    else:
+        # Assume it's smaller than or = 5
+        return data[1]
 
-   if isinstance(term, unicode): 
-      term = term.encode('utf-8')
-   else: term = term.decode('utf-8')
 
-   term = term.replace('_', ' ')
-   try: uri = search.google_search('site:en.wikipedia.org %s' % term)
-   except IndexError: return term
-   if uri: 
-      return uri[len('http://en.wikipedia.org/wiki/'):]
-   else: return term
+def wikiDefine(term, url):
+    """Use MediaWikis API to define a value from wiktionary and wikipedia"""
+    # First, we need to grab the data, and serialize it in JSON
+    url_query = urlify(term)
+    data = jsonify(get(full_define % (lang, url, maxlen, url_query)).read())['query']['pages']
 
-def wikipedia(term, language='en', last=False): 
-   global wikiuri
-   if not '%' in term: 
-      if isinstance(term, unicode): 
-         t = term.encode('utf-8')
-      else: t = term
-      q = urllib.quote(t)
-      u = wikiuri % (language, q)
-      bytes = web.get(u)
-   else: bytes = web.get(wikiuri % (language, term))
+    # We need to see if it was found. If it wasn't it'll be a -1 page
+    for pageNumber, pageData in data.iteritems():
+        if pageNumber == '-1':
+            # Assume failed to find
+            return False
+        else:
+            # Assume found a result. Now, find and return the title/contents.
+            if pageData['extract'].startswith('REDIRECT'):
+                return False # This means it's a redirect page according to MediaWiki API
+            title = pageData['title']
+            content = pageData['extract'].encode('ascii', 'ignore').replace('\n', ' ')
+            while '  ' in content:
+                content = content.replace('  ', ' ')
+            return [title, content]
 
-   if bytes.startswith('\x1f\x8b\x08\x00\x00\x00\x00\x00'): 
-      f = StringIO.StringIO(bytes)
-      f.seek(0)
-      gzip_file = gzip.GzipFile(fileobj=f)
-      bytes = gzip_file.read()
-      gzip_file.close()
-      f.close()
 
-   bytes = r_tr.sub('', bytes)
+def wikipedia(code, input):
+    """Pull definitions/search from wikipedia.org"""
+    if not input.group(2):
+        return code.say('The syntax is: \'.wiki <word>\'')
+    try:
+        # Try to get the definition of what they WANT
+        data = wikiDefine(input.group(2).strip(), wikipedia_url) # 1:title, 2:content
+        if not data:
+            # Assume it's working, just no definition..
+            # Try and give them search results
+            search = wikiSearch(input.group(2).strip(), wikipedia_url) # list() of results
+            if not search:
+                # Crap. No results for this either?
+                return code.say('No Wiki page for %s! (Try \'.wsearch\')' % input.group(2).strip())
+            else:
+                # Make nice sexy results, because we're sorry we couldn't find it...
+                return code.say('Unable to find results. Try: %s' % code.color('purple', ', '.join(search)))
+        else:
+            # Working, print out a nice clean definition.
+            print repr(data[1])
+            return code.say('%s: %s' % (code.color('purple', data[0]), data[1]))
+    except:
+        return code.say('Failed to get the definition!')
+wikipedia.cmds = ['wiki', 'w']
+wikipedia.priority = 'medium'
 
-   if not last: 
-      r = r_redirect.search(bytes[:4096])
-      if r: 
-         term = urllib.unquote(r.group(1))
-         return wikipedia(term, language=language, last=True)
+def wikipediaSearch(code, input):
+    """Pull search results from wikipedia.org"""
+    if not input.group(2):
+        return code.say('The syntax is: \'.wsearch <word(s)>\'')
+    try:
+        search = wikiSearch(input.group(2).strip(), wikipedia_url) # list() of results
+        if not search:
+            # Crap. No results for this either?
+            return code.say('No Wiki page for %s!' % code.color('purple', input.group(2).strip()))
+        else:
+            # Make nice sexy results, because we're sorry we couldn't find it...
+            return code.say('Suggestions: %s' % code.color('purple', ', '.join(search)))
+    except:
+        return code.say(code.color('red', 'No suggestions found!'))
+wikipediaSearch.cmds = ['wdefine', 'wsearch','wikisearch','swiki']
+wikipediaSearch.priority = 'medium'
 
-   paragraphs = r_paragraph.findall(bytes)
-
-   if not paragraphs: 
-      if not last: 
-         term = search(term)
-         return wikipedia(term, language=language, last=True)
-      return None
-
-   # Pre-process
-   paragraphs = [para for para in paragraphs 
-                 if (para and 'technical limitations' not in para 
-                          and 'window.showTocToggle' not in para 
-                          and 'Deletion_policy' not in para 
-                          and 'Template:AfD_footer' not in para 
-                          and not (para.startswith('<p><i>') and 
-                                   para.endswith('</i></p>'))
-                          and not 'disambiguation)"' in para) 
-                          and not '(images and media)' in para
-                          and not 'This article contains a' in para 
-                          and not 'id="coordinates"' in para
-                          and not 'class="thumb' in para]
-                          # and not 'style="display:none"' in para]
-
-   for i, para in enumerate(paragraphs): 
-      para = para.replace('<sup>', '|')
-      para = para.replace('</sup>', '|')
-      paragraphs[i] = text(para).strip()
-
-   # Post-process
-   paragraphs = [para for para in paragraphs if 
-                 (para and not (para.endswith(':') and len(para) < 150))]
-
-   para = text(paragraphs[0])
-   m = r_sentence.match(para)
-
-   if not m: 
-      if not last: 
-         term = search(term)
-         return wikipedia(term, language=language, last=True)
-      return None
-   sentence = m.group(0)
-
-   maxlength = 275
-   if len(sentence) > maxlength: 
-      sentence = sentence[:maxlength]
-      words = sentence[:-5].split(' ')
-      words.pop()
-      sentence = ' '.join(words) + ' [...]'
-
-   if (('using the Article Wizard if you wish' in sentence)
-    or ('or add a request for it' in sentence)
-    or ('in existing articles' in sentence)): 
-      if not last: 
-         term = search(term)
-         return wikipedia(term, language=language, last=True)
-      return None
-
-   sentence = '"' + sentence.replace('"', "'") + '"'
-   sentence = sentence.decode('utf-8').encode('utf-8')
-   wikiuri = wikiuri.decode('utf-8').encode('utf-8')
-   term = term.decode('utf-8').encode('utf-8')
-   return sentence + ' - ' + (wikiuri % (language, term))
-
-def wik(code, input): 
-   origterm = input.groups()[1]
-   if not origterm: 
-      return code.say('Perhaps you meant ".wik Zen"?')
-   origterm = origterm.encode('utf-8')
-   origterm = origterm.strip()
-
-   term = urllib.unquote(origterm)
-   language = 'en'
-   if term.startswith(':') and (' ' in term): 
-      a, b = term.split(' ', 1)
-      a = a.lstrip(':')
-      if a.isalpha(): 
-         language, term = a, b
-   term = term[0].upper() + term[1:]
-   term = term.replace(' ', '_')
-
-   try: result = wikipedia(term, language)
-   except IOError: 
-      args = (language, wikiuri % (language, term))
-      error = "Can't connect to %s.wikipedia.org (%s)" % args
-      return code.say(error)
-
-   if result is not None: 
-      code.say(result)
-   else: code.say('Can\'t find anything in Wikipedia for "%s".' % origterm)
-
-wik.commands = ['wik', 'wiki', 'define2', 'dict2']
-wik.priority = 'high'
 
 if __name__ == '__main__': 
-   print __doc__.strip()
+    print __doc__.strip()
