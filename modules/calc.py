f077222d58edf97ed1ccd510e216215656b1e2b3
Liam Stanley <me@liamstanley.io>
2014-03-27 00:36:03 -0400

    Add hashing features. (vitganster99 you're welcome :+1:)

--- modules/calc.py
+++ modules/calc.py
@@ -1,65 +1,78 @@
 #!/usr/bin/env python
-# coding=utf-8
 """
 Code Copyright (C) 2012-2014 Liam Stanley
-calc.py - Code Calculator Module
+calc.py - Code Calculations Module
 http://code.liamstanley.io/
 """
 
 import re, json
 import urllib, urllib2
+import hashlib
 from util.hook import *
+
 
 uri = 'http://api.duckduckgo.com/?q=%s&format=json'
 
+
+@hook(cmds=['c','calc','calculate'], ex='calc 5 + 3', args=True)
 def calc(code, input):
-    if empty(code, input): return
     try:
         data = json.loads(urllib2.urlopen(uri % urllib.quote(input.group(2).replace('^','**'))).read())
         if data['AnswerType'] != 'calc':
             return code.reply('Failed to calculate')
-        #print data['Answer']
         answer = re.sub(r'\<.*?\>', '', data['Answer']).strip()
         return code.say(answer)
     except:
         return code.reply('Failed to calculate!')
-calc.commands = ['c', 'calc', 'calculate']
-calc.example = 'calc 5 + 3'
 
+
+@hook(cmds=['py','python'], ex='py print(int(1.0) + int(3))', args=True)
 def py(code, input):
-    # Prevention from spam, along with exploits on Atheme/Charybdis networks
-    # (see line 41, new fix)
-    if empty(code, input): return
+    """python <commands> -- Execute Python inside of a sandbox"""
     query = input.group(2).encode('utf-8')
     uri = 'http://tumbolia.appspot.com/py/'
     try:
-         answer = urllib2.urlopen(uri + urllib.quote(query)).read()
-         if answer:
-              # Strip line endings from the response, to prevent cut-off
-              # Of multi-line responses
-              answer = answer.replace('\n',' ').replace('\t',' ').replace('\r','')
-              # Make sure to reply result, so we don't run into Fantasy-prefix
-              # Channel command issues/security holes
-              code.reply(answer)
-         else:
-              code.reply('Sorry, no {b}%s{b}')
+        answer = urllib2.urlopen(uri + urllib.quote(query)).read()
+        if answer:
+            answer = answer.replace('\n',' ').replace('\t',' ').replace('\r','')
+            return code.reply(answer)
+        else:
+            return code.reply('Sorry, no {b}%s{b}')
     except Exception, e:
-         code.reply('{red}The server did not return an answer.')
-         print '[.py]', e
-py.commands = ['py', 'python']
-py.example = 'py print(int(1.0) + int(3))'
+        return code.reply('{red}The server did not return an answer.')
 
+
+@hook(cmds=['wa'], ex='wa 1 mile in feet', args=True)
 def wa(code, input):
     """Wolphram Alpha search"""
-    if empty(code, input): return
     query = input.group(2).encode('utf-8')
     uri = 'http://tumbolia.appspot.com/wa/'
     answer = urllib2.urlopen(uri + urllib.quote(query.replace('+', '%2B'))).read()
-    if answer: 
-        code.say(answer)
-    else: code.reply('Sorry, no result.')
+    if answer:
+        return code.say(answer)
+    else:
+        return code.reply('Sorry, no result.')
 wa.commands = ['wa']
 wa.example = 'wa 1 mile in feet'
+
+
+@hook(cmds=['md5', 'hash'], priority='low', args=True)
+def md5(code, input):
+    """md5 <string> -- Create a md5 hash of the input string"""
+    return code.say(hashlib.md5(input.group(2)).hexdigest())
+
+
+@hook(cmds=['sha256'], priority='low', args=True)
+def sha256(code, input):
+    """sha256 <string> -- Create a sha256 hash of the input string"""
+    return code.say(hashlib.sha256(input.group(2)).hexdigest())
+
+
+@hook(cmds=['sha512'], priority='low', args=True)
+def sha512(code, input):
+    """sha512 <string> -- Create a sha512 hash of the input string"""
+    return code.say(hashlib.sha512(input.group(2)).hexdigest())
+
 
 if __name__ == '__main__': 
     print __doc__.strip()
