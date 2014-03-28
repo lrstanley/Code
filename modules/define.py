978318998d80c43289c7b81423aa0db0987f1c85
Liam Stanley <me@liamstanley.io>
2014-03-28 17:28:04 -0400

    Fix define.py, add colors, fix HTTPError, no more utf-8

--- modules/define.py
+++ modules/define.py
@@ -15,7 +15,7 @@ uri = 'http://en.wiktionary.org/w/index.php?title=%s&printable=yes'
 r_tag = re.compile(r'<[^>]+>')
 r_ul = re.compile(r'(?ims)<ul>.*?</ul>')
 
-def text(html): 
+def text(html):
    text = r_tag.sub('', html).strip()
    text = text.replace('\n', ' ')
    text = text.replace('\r', '')
@@ -23,75 +23,76 @@ def text(html):
    text = text.replace('(transitive', '(trans.')
    return text
 
-def wiktionary(word): 
-   bytes = urlopen(uri % quote(word.encode('utf-8'))).read()
+def wiktionary(word):
+   try:
+      bytes = urlopen(uri % quote(word.encode('utf-8'))).read()
+   except:
+      return False, False
    bytes = r_ul.sub('', bytes)
 
    mode = None
    etymology = None
    definitions = {}
-   for line in bytes.splitlines(): 
-      if 'id="Etymology"' in line: 
+   for line in bytes.splitlines():
+      if 'id="Etymology"' in line:
          mode = 'etymology'
-      elif 'id="Noun"' in line: 
+      elif 'id="Noun"' in line:
          mode = 'noun'
-      elif 'id="Verb"' in line: 
+      elif 'id="Verb"' in line:
          mode = 'verb'
-      elif 'id="Adjective"' in line: 
+      elif 'id="Adjective"' in line:
          mode = 'adjective'
-      elif 'id="Adverb"' in line: 
+      elif 'id="Adverb"' in line:
          mode = 'adverb'
-      elif 'id="Interjection"' in line: 
+      elif 'id="Interjection"' in line:
          mode = 'interjection'
-      elif 'id="Particle"' in line: 
+      elif 'id="Particle"' in line:
          mode = 'particle'
-      elif 'id="Preposition"' in line: 
+      elif 'id="Preposition"' in line:
          mode = 'preposition'
-      elif 'id="' in line: 
+      elif 'id="' in line:
          mode = None
 
-      elif (mode == 'etmyology') and ('<p>' in line): 
+      elif (mode == 'etmyology') and ('<p>' in line):
          etymology = text(line)
-      elif (mode is not None) and ('<li>' in line): 
+      elif (mode is not None) and ('<li>' in line):
          definitions.setdefault(mode, []).append(text(line))
 
-      if '<hr' in line: 
+      if '<hr' in line:
          break
    return etymology, definitions
 
-parts = ('preposition', 'particle', 'blue', 'noun', 'verb', 
+parts = ('preposition', 'particle', 'blue', 'noun', 'verb',
    'adjective', 'adverb', 'interjection')
 
 def format(word, definitions, number=2):
-   result = '%s' % word.encode('utf-8')
-   for part in parts: 
-      if definitions.has_key(part): 
+   result = '{purple}{b}%s{b}{c}' % word.encode('utf-8')
+   for part in parts:
+      if definitions.has_key(part):
          defs = definitions[part][:number]
-         result += u' \u2014 '.encode('utf-8') + ('%s: ' % part)
+         result += u' \u2014 ' + ('{blue}{b}%s{c}{b}: ' % part)
          n = ['%s. %s' % (i + 1, e.strip(' .')) for i, e in enumerate(defs)]
          result += ', '.join(n)
    return result.strip(' .,')
 
-def w(code, input): 
+def w(code, input):
    if empty(code, input): return
    word = input.group(2).lower()
    etymology, definitions = wiktionary(word)
-   if not definitions: 
+   if not definitions:
       code.say('{red}Couldn\'t get any definitions for {b}%s{b}.' % word)
       return
 
-   result = format('{purple}{b}' + word + '{c}{b}', definitions)
-   if len(result) < 150: 
+   result = format(word, definitions)
+   if len(result) < 150:
       result = format(word, definitions, 3)
-#   if len(result) < 150: 
-#      result = format(word, definitions, 5)
 
-   if len(result) > 300: 
-      result = result[:295] + '[...]'
+   if len(result) > 300:
+      result = result[:294] + ' {red}{b}[...]'
    code.say(result)
 w.commands = ['w','define', 'd']
 w.example = 'w example'
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
    print __doc__.strip()
