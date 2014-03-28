8922d0b5868cbbc4a7bbfcce258f8a4feb53d3f4
Liam Stanley <me@liamstanley.io>
2014-03-28 01:18:55 -0400

    Immprove/remove broken features from translate.py

--- modules/translate.py
+++ modules/translate.py
@@ -2,7 +2,6 @@
 # coding=utf-8
 """
 Code Copyright (C) 2012-2014 Liam Stanley
-Credits: Sean B. Palmer, Michael Yanovich
 translate.py - Code Translation Module
 http://code.liamstanley.io/
 """
@@ -14,7 +13,8 @@ import urllib2
 import util.web
 from util.hook import *
 
-def translate(text, input='auto', output='en', use_proxy=False):
+
+def translate(text, input='auto', output='en'):
     raw = False
     if output.endswith('-raw'):
         output = output[:-4]
@@ -51,12 +51,7 @@ def translate(text, input='auto', output='en', use_proxy=False):
     for x in params:
         uri += '&%s=%s' % (x, params[x])
 
-    if use_proxy:
-        print 'USING PROXY'
-        result = proxy.get(uri)
-    else:
-        print 'NOT USING PROXY'
-        result = opener.open(uri).read()
+    result = opener.open(uri).read()
 
     ## this is hackish
     ## this makes the returned data parsable by the json module
@@ -70,7 +65,7 @@ def translate(text, input='auto', output='en', use_proxy=False):
         return str(data), 'en-raw'
 
     try:
-        language = data[2] # -2][0][0]
+        language = data[2]
     except:
         language = '?'
 
@@ -79,38 +74,10 @@ def translate(text, input='auto', output='en', use_proxy=False):
 
     return ''.join(x[0] for x in data[0]), language
 
-def tr(code, context):
+
+@hook(cmds=['tr', 'translate'], args=True)
+def tr(code, input):
     """Translates a phrase, with an optional language hint."""
-    input, output, phrase = context.groups()
-
-    phrase = phrase.encode('utf-8')
-
-    if (len(phrase) > 350) and (not context.admin):
-        return code.reply('Phrase must be under 350 characters.')
-
-    input = input or 'auto'
-    input = input.encode('utf-8')
-    output = (output or 'en').encode('utf-8')
-
-    if input != output:
-        msg, input = translate(phrase, input, output)
-        if isinstance(msg, str):
-            msg = msg.decode('utf-8')
-        if msg:
-            msg = util.web.decode(msg) # msg.replace('&#39;', "'")
-            msg = '"%s" (%s to %s)' % (msg, input, output)
-        else: msg = 'The %s to %s translation failed, sorry!' % (input, output)
-
-        code.reply(msg)
-    else: code.reply('Language guessing failed, so try suggesting one!')
-
-tr.rule = ('$nick', ur'(?:([a-z]{2}) +)?(?:([a-z]{2}|en-raw) +)?["“](.+?)["”]\? *$')
-tr.example = '$nickname: "mon chien"? or $nickname: fr "mon chien"?'
-tr.priority = 'low'
-
-def tr2(code, input):
-    """Translates a phrase, with an optional language hint."""
-    if empty(code, input): return
     command = input.group(2).encode('utf-8')
 
     def langcode(p):
@@ -127,7 +94,7 @@ def tr2(code, input):
     phrase = command
 
     if (len(phrase) > 350) and (not input.admin):
-        return code.reply('Phrase must be under 350 characters.')
+        return code.say('{red}Phrase must be under 350 characters.')
 
     src, dest = args
     if src != dest:
@@ -135,35 +102,13 @@ def tr2(code, input):
         if isinstance(msg, str):
             msg = msg.decode('utf-8')
         if msg:
-            msg = util.web.decode(msg) # msg.replace('&#39;', "'")
-            msg = '"%s" (%s to %s)' % (msg, src, dest)
-        else: msg = 'The %s to %s translation failed, sorry!' % (src, dest)
+            msg = util.web.decode(msg)
+            msg = '"%s" ({b}{purple}%s{c}{b} to {b}{purple}%s{c}{b})' % (msg, src, dest)
+        else: msg = '{red}The %s to %s translation failed, sorry!' % (src, dest)
 
         code.reply(msg)
-    else: code.reply('Language guessing failed, so try suggesting one!')
+    else: code.say('{red}Language guessing failed, so try suggesting one!')
 
-tr2.commands = ['tr']
-tr2.priority = 'low'
-
-def mangle(code, input):
-    phrase = input.group(2).encode('utf-8')
-    for lang in ['fr', 'de', 'es', 'it', 'ja']:
-        backup = phrase
-        phrase = translate(phrase, 'en', lang)
-        if not phrase:
-            phrase = backup
-            break
-        __import__('time').sleep(0.5)
-
-        backup = phrase
-        phrase = translate(phrase, lang, 'en')
-        if not phrase:
-            phrase = backup
-            break
-        __import__('time').sleep(0.5)
-
-    code.reply(phrase or 'ERRORS D:')
-mangle.commands = ['mangle']
 
 if __name__ == '__main__':
     print __doc__.strip()
