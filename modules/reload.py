9bd7d5e371e96f2da68e37b483cef1aabac30a00
Liam Stanley <me@liamstanley.io>
2014-03-28 17:47:22 -0400

    Update reload.py for colors and such

--- modules/reload.py
+++ modules/reload.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """
 Code Copyright (C) 2012-2014 Liam Stanley
-Credits: Sean B. Palmer, Michael Yanovich
-reload.py - Code Module Reloader Module
+reload.py - Code Reload Module
 http://code.liamstanley.io/
 """
 
@@ -10,80 +9,61 @@ import sys, os.path, time, imp, subprocess
 from core import irc
 from util.hook import *
 
-def f_reload_cmd(code, input):
-    f_reload(code, input)
-f_reload_cmd.commands = ['rld', 'reload', 'recompile']
 
-def f_reload(code, input): 
-    """Reloads a module, for use by admins only.""" 
-    if not admin(code, input): return
+@hook(cmds=['reload', 'rld'], priority='high', thread=False, admin=True)
+def f_reload(code, input):
+    """Reloads a module, for use by admins only."""
 
     name = input.group(2)
-    if name == code.config.owner: 
+    if name == code.config.owner:
         return code.reply('What?')
 
-    if (not name) or (name == '*'): 
+    if (not name) or (name == '*'):
         code.variables = None
         code.commands = None
         code.setup()
-        return code.reply(code.bold('Reloaded all modules.'))
+        return code.reply('{b}Reloaded all modules.')
 
     # if a user supplies the module with the extension
     if name.endswith('.py'):
         name = os.path.splitext(name)[0]
 
-    if not sys.modules.has_key(name): 
-        return code.reply('%s: no such module!' % code.bold(name))
+    if not sys.modules.has_key(name):
+        return code.reply('%s: No such module!' % code.bold(name))
 
     # Thanks to moot for prodding me on this
     path = sys.modules[name].__file__
-    if path.endswith('.pyc') or path.endswith('.pyo'): 
+    if path.endswith('.pyc') or path.endswith('.pyo'):
         path = path[:-1]
-    if not os.path.isfile(path): 
+    if not os.path.isfile(path):
         return code.reply('Found %s, but not the source file' % code.bold(name))
 
     module = imp.load_source(name, path)
     sys.modules[name] = module
-    if hasattr(module, 'setup'): 
+    if hasattr(module, 'setup'):
         module.setup(code)
 
     mtime = os.path.getmtime(module.__file__)
-    modified = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(mtime))
+    modified = time.strftime('%H:%M:%S', time.gmtime(mtime))
 
     code.register(vars(module))
     code.bind_commands()
-
-    code.reply('%r (version: %s)' % (module, modified)) # no colorcodes, weird shit happens!
-f_reload.rule = ('$nick', ['reload'], r'(\S+)?')
-f_reload.priority = 'low'
-f_reload.thread = False
-f_reload.rate = 20
+    module = str(module)
+    module_name, module_location = module.split()[1].strip('\''), module.split()[3].strip('\'')
+    code.say('{b}Reloaded {blue}%s{c} (from {blue}%s{c}) (version: %s){b}' % (module_name, module_location, modified))
 
 
-if sys.version_info >= (2, 7):
-    def update(code, input):
-        if not input.admin:
-              return
+@hook(cmds=['update'], rate=30, admin=True)
+def update(code, input):
+    """Pulls the latest versions of all modules from Git"""
+    if not sys.platform.startswith('linux'):
+        code.say('Warning: %s' % code.bold('Using a non-linux OS, might fail to work!'))
+    proc = subprocess.Popen('git pull', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    data = proc.communicate()[0]
+    while '  ' in data:
+        data = data.replace('  ', ' ')
+    code.say('Github: ' + code.bold(data))
+    f_reload(code, input)
 
-        """Pulls the latest versions of all modules from Git"""
-        if not sys.platform.startswith('linux'):
-            code.say('Warning: %s' % code.bold('Using a non-linux OS, might fail to work!'))
-        # normally use '/usr/bin/git' for the command, but to support windows, 
-        # we might be able to do it this way
-        proc = subprocess.Popen('git pull',
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE, shell=True)
-        data = proc.communicate()[0]
-        while '  ' in data:
-            data = data.replace('  ', ' ')
-        code.say('Github: ' + code.bold(data))
-        f_reload(code, input)
-else:
-    def update(code, input):
-        code.reply('You need to run me on %s to do that.' % code.bold(code.color('red', 'Python 2.7')))
-#update.rule = ('$nick', ['update'], r'(.+)')
-update.commands = ['update']
-update.rate = 30
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     print __doc__.strip()
