3ac56cfd942dc6492e066c0d00deb4c732a9678a
Liam Stanley <me@liamstanley.io>
2014-05-18 13:31:58 +0000

    unicode.py search overhaul

--- modules/unicode.py
+++ modules/unicode.py
@@ -1,8 +1,8 @@
 import re
 import unicodedata
-import urlparse
-from itertools import islice
+import difflib
 from util.hook import *
+from util.web import get
 
 all_chars = (unichr(i) for i in xrange(0x110000))
 control_chars = ''.join(map(unichr, range(0, 32) + range(127, 160)))
@@ -50,151 +50,68 @@ def encode(bytes):
     return text
 
 
-def urlEncodeNonAscii(b):
-    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
-
-
-def iriToUri(iri):
-    parts = urlparse.urlparse(iri)
-    return urlparse.urlunparse(
-        part.encode('idna') if parti == 1 else urlEncodeNonAscii(part.encode('utf-8'))
-        for parti, part in enumerate(parts)
-    )
-
-
 def remove_control_chars(s):
     return control_char_re.sub('', s)
 
 
-def about(u, cp=None, name=None):
-    if cp is None:
-        cp = ord(u)
-    if name is None:
-        try:
-            name = unicodedata.name(u)
-        except ValueError:
-            return 'U+%04X (No name found)' % cp
-
-    if not unicodedata.combining(u):
-        template = 'U+%04X %s (%s)'
-    else:
-        template = 'U+%04X %s (\xe2\x97\x8c%s)'
-    return template % (cp, name, u.encode('utf-8'))
-
-
-def codepoint_simple(arg):
-    arg = arg.upper()
-
-    r_label = re.compile('\\b' + arg.replace(' ', '.*\\b') + '\\b')
-
-    results = []
-    for cp in xrange(0xFFFF):
-        u = unichr(cp)
-        try:
-            name = unicodedata.name(u)
-        except ValueError:
-            continue
-
-        if r_label.search(name):
-            results.append((len(name), u, cp, name))
-    if not results:
-        r_label = re.compile('\\b' + arg.replace(' ', '.*\\b'))
-        for cp in xrange(0xFFFF):
-            u = unichr(cp)
-            try:
-                name = unicodedata.name(u)
-            except ValueError:
-                continue
-
-            if r_label.search(name):
-                results.append((len(name), u, cp, name))
-
-    if not results:
-        return None
-
-    length, u, cp, name = sorted(results)[0]
-    return about(u, cp, name)
-
-
-def codepoint_extended(arg):
-    arg = arg.upper()
-    try:
-        r_search = re.compile(arg)
-    except:
-        raise ValueError('Broken regexp: %r' % arg)
-
-    for cp in xrange(1, 0x10FFFF):
-        u = unichr(cp)
-        name = unicodedata.name(u, '-')
-
-        if r_search.search(name):
-            yield about(u, cp, name)
-
-
 @hook(cmds=['u'], ex='u 203D')
-def u(code, input):
+def u(code, input, search=False):
     """Look up unicode information."""
     arg = input.bytes[3:]
     if not arg:
         return code.reply('You gave me zero length input.')
-    elif not arg.strip(' '):
-        if len(arg) > 1:
-            return code.reply('%s SPACEs (U+0020)' % len(arg))
-        return code.reply('1 SPACE (U+0020)')
+    data = get('http://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt').read()
+    data = data.split('\n')
+    del data[-1]
+    uc = {}
+    uc_names, cp_names = [], {}
+    # http://www.unicode.org/reports/tr44/#UnicodeData.txt
+    for line in data:
+        tmp = line.split(';')
+        name = tmp[1]
+        if tmp[10]:
+            name = name + ' ' + str(tmp[10])
+        uc[name] = tmp
+        uc_names.append(name)
+        cp_names[tmp[0]] = name
 
-    # @@ space
-    if set(arg.upper()) - set(
-        'ABCDEFGHIJKLMNOPQRSTUVWYXYZ0123456789- .?+*{}[]\\/^$'
-    ):
-        printable = False
-    elif len(arg) > 1:
-        printable = True
-    else:
-        printable = False
+    found = difflib.get_close_matches(arg, uc_names)
+    if not found:
+        found = difflib.get_close_matches(arg.upper(), uc_names)
+    if found:
+        if search:
+            return code.say('{b}Possible matches:{b} %s' % ', '.join(found))
+        char = uc[list(found)[0]]
+    if not found:
+        # Try the codepoint search too...
+        if arg.upper() in cp_names:
+            char = uc[cp_names[arg.upper()]]
+        else:
+            return code.reply('I can\'t seem to find that Unicode!')
+    msg = u'{} (U+{}) '.format(char[1], char[0])
+    msg += '"' + u_converter('\u' + char[0]) + '"'
+    code.say(msg)
 
-    if printable:
-        extended = False
-        for c in '.?+*{}[]\\/^$':
-            if c in arg:
-                extended = True
-                break
 
-        if len(arg) == 4:
+@hook(cmds=['ul'], ex='u SPACE')
+def ul(code, input):
+    u(code, input, search=True)
+
+def u_converter(string):
+    chars = string.split('\u')
+    chinese = ''
+    for char in chars:
+        if len(char):
             try:
-                u = unichr(int(arg, 16))
+                ncode = int(char,16)
             except ValueError:
-                pass
-            else:
-                return code.say(about(u))
-
-        if extended:
-            # look up a codepoint with regexp
-            results = list(islice(codepoint_extended(arg), 4))
-            for i, result in enumerate(results):
-                if (i < 2) or ((i == 2) and (len(results) < 4)):
-                    code.say(result)
-                elif (i == 2) and (len(results) > 3):
-                    code.say(result + ' [...]')
-            if not results:
-                code.reply('Sorry, no results')
-        else:
-            # look up a codepoint freely
-            result = codepoint_simple(arg)
-            if result is not None:
-                code.say(result)
-            else:
-                code.reply('{red}Sorry, no results for %r.' % arg)
-    else:
-        text = arg.decode('utf-8')
-        # look up less than three codepoints
-        if len(text) <= 3:
-            for u in text:
-                code.say(about(u))
-        # look up more than three codepoints
-        elif len(text) <= 10:
-            code.reply(' '.join('U+%04X' % ord(c) for c in text))
-        else:
-            code.reply('{red}Sorry, your input is too long!')
+                continue
+            try:
+                uchar = unichr(ncode)
+            except ValueError:
+                continue
+            chinese += uchar
+    return chinese
 
 
 @hook(cmds=['bytes'], ex='bytes \xe3\x8b\xa1', args=True)
