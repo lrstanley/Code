9504a613367e4b5f2208f3054fdcf5206d824d72
Liam Stanley <me@liamstanley.io>
2014-05-18 11:40:11 -0400

    Overhaul define.py, remove test.py

--- modules/define.py
+++ modules/define.py
@@ -1,90 +1,32 @@
-import re
-from urllib import quote
-from urllib2 import urlopen
+from util import web
 from util.hook import *
 
 
-uri = 'http://en.wiktionary.org/w/index.php?title=%s&printable=yes'
-r_tag = re.compile(r'<[^>]+>')
-r_ul = re.compile(r'(?ims)<ul>.*?</ul>')
-parts = (
-    'preposition', 'particle', 'blue', 'noun', 'verb',
-    'adjective', 'adverb', 'interjection'
-)
+uri = 'http://ninjawords.com/definitions/get/%s'
 
 
-def text(html):
-    text = r_tag.sub('', html).strip()
-    text = text.replace('\n', ' ')
-    text = text.replace('\r', '')
-    text = text.replace('(intransitive', '(intr.')
-    text = text.replace('(transitive', '(trans.')
-    return text
-
-
-def wiktionary(word):
+@hook(cmds=['define', 'w', 'd'], ex='define example', args=True)
+def define(code, input):
     try:
-        bytes = urlopen(uri % quote(word.encode('utf-8'))).read()
+        data = web.json(uri % web.quote(input.group(2)))[0]
     except:
-        return False, False
-    bytes = r_ul.sub('', bytes)
+        return code.reply('{red}Failed to get definition!')
 
-    mode = None
-    etymology = None
-    definitions = {}
-    for line in bytes.splitlines():
-        if 'id="Etymology"' in line:
-            mode = 'etymology'
-        elif 'id="Noun"' in line:
-            mode = 'noun'
-        elif 'id="Verb"' in line:
-            mode = 'verb'
-        elif 'id="Adjective"' in line:
-            mode = 'adjective'
-        elif 'id="Adverb"' in line:
-            mode = 'adverb'
-        elif 'id="Interjection"' in line:
-            mode = 'interjection'
-        elif 'id="Particle"' in line:
-            mode = 'particle'
-        elif 'id="Preposition"' in line:
-            mode = 'preposition'
-        elif 'id="' in line:
-            mode = None
+    # Go through filters to remove extra stuff that's not needed.
+    word = data['html']
+    word = web.striptags(word).strip()
+    word = web.htmlescape(word)
+    word = word.replace('\\n', '').replace('\n', '')
+    while '  ' in word:
+        word = word.replace('  ', ' ')
 
-        elif (mode == 'etmyology') and ('<p>' in line):
-            etymology = text(line)
-        elif (mode is not None) and ('<li>' in line):
-            definitions.setdefault(mode, []).append(text(line))
+    word = word.encode('ascii', 'ignore')
+    if 'is not in the dictionary.' in word:
+        return code.say('Definition for {b}%s{b} not found' % input.group(2))
 
-        if '<hr' in line:
-            break
-    return etymology, definitions
-
-
-def format(word, definitions, number=2):
-    result = '{purple}{b}%s{b}{c}' % word.encode('utf-8')
-    for part in parts:
-        if part in definitions:
-            defs = definitions[part][:number]
-            result += u' \u2014 ' + ('{blue}{b}%s{c}{b}: ' % part)
-            n = ['%s. %s' % (i + 1, e.strip(' .')) for i, e in enumerate(defs)]
-            result += ', '.join(n)
-    return result.strip(' .,')
-
-
-@hook(cmds=['w', 'define', 'd'], ex='w example', args=True)
-def w(code, input):
-    word = input.group(2).lower()
-    etymology, definitions = wiktionary(word)
-    if not definitions:
-        code.say('{red}Couldn\'t get any definitions for {b}%s{b}.' % word)
-        return
-
-    result = format(word, definitions)
-    if len(result) < 150:
-        result = format(word, definitions, 3)
-
-    if len(result) > 300:
-        result = result[:294] + ' [...]'
-    code.say(result)
+    # Everything below here is for colors only
+    word = '{b}{purple}%s{c}{b}: %s' % (data['query'], word[len(data['query']) + 1::])
+    word = word.replace('(', '{purple}{b}(').replace(')', '){b}{c}')
+    if len(word) > 250:
+        word = word[:245] + '{c}{b}[...]'
+    code.say(word)
--- modules/test.py
+++
@@ -1,6 +0,0 @@
-from util.hook import *
-
-
-@hook(rule=r'.*', event='004')
-def test(code, input):
-    print 'THIS IS A TEST'
