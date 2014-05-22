0b7d9b7a3f3cd977f1cda5751bddb6fa2bb8e26d
Liam Stanley <me@liamstanley.io>
2014-05-22 12:27:01 +0000

    Redo unicode.py

--- modules/unicode.py
+++ modules/unicode.py
@@ -1,8 +1,11 @@
 import re
+import time
 import unicodedata
 import difflib
 from util.hook import *
 from util.web import get
+from util import database
+from util import output
 
 all_chars = (unichr(i) for i in xrange(0x110000))
 control_chars = ''.join(map(unichr, range(0, 32) + range(127, 160)))
@@ -54,27 +57,16 @@ def remove_control_chars(s):
     return control_char_re.sub('', s)
 
 
-@hook(cmds=['u'], ex='u 203D')
+uc_names, cp_names, uc = [], {}, {}
+
+
+@hook(cmds=['u'], ex='u SPACE')
 def u(code, input, search=False):
     """Look up unicode information."""
-    arg = input.bytes[3:]
+    global uc_names, cp_names, uc
+    arg = repr(input.group(2)).replace('\u', '').replace('u', '').replace('\'', '')
     if not arg:
         return code.reply('You gave me zero length input.')
-    data = get('http://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt').read()
-    data = data.split('\n')
-    del data[-1]
-    uc = {}
-    uc_names, cp_names = [], {}
-    # http://www.unicode.org/reports/tr44/#UnicodeData.txt
-    for line in data:
-        tmp = line.split(';')
-        name = tmp[1]
-        if tmp[10]:
-            name = name + ' ' + str(tmp[10])
-        uc[name] = tmp
-        uc_names.append(name)
-        cp_names[tmp[0]] = name
-
     found = difflib.get_close_matches(arg, uc_names)
     if not found:
         found = difflib.get_close_matches(arg.upper(), uc_names)
@@ -93,8 +85,42 @@ def u(code, input, search=False):
     code.say(msg)
 
 
-@hook(cmds=['ul'], ex='u SPACE')
-def ul(code, input):
+def gen_db(botname):
+    global uc_names, cp_names, uc
+    # http://www.unicode.org/reports/tr44/#UnicodeData.txt
+    output.info('Downloading Unicode data')
+    data = get('http://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt').read()
+    data = data.split('\n')
+    del data[-1]
+    # http://www.unicode.org/reports/tr44/#UnicodeData.txt
+    for line in data:
+        tmp = line.split(';')
+        name = tmp[1]
+        if tmp[10]:
+            name = name + ' ' + str(tmp[10])
+        uc[name] = tmp
+        uc_names.append(name)
+        cp_names[tmp[0]] = name
+    database.set(botname, {'uc': uc, 'uc_names': uc_names, 'cp_names': cp_names, 'time': int(time.time())}, 'unicodedata')
+
+def setup(code):
+    global uc_names, cp_names, uc
+    curr = int(time.time())
+    db = database.get(code.nick, 'unicodedata')
+    if not db:
+        gen_db(code.nick)
+    else:
+        diff = int(curr - int(db['time']))
+        if diff > 518400:
+            gen_db(code.nick)
+            return
+        uc = db['uc']
+        uc_names = db['uc_names']
+        cp_names = db['cp_names']
+
+
+@hook(cmds=['us'], ex='us SPACE')
+def us(code, input):
     u(code, input, search=True)
 
 def u_converter(string):
@@ -103,7 +129,7 @@ def u_converter(string):
     for char in chars:
         if len(char):
             try:
-                ncode = int(char,16)
+                ncode = int(char, 16)
             except ValueError:
                 continue
             try:
