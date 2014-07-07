09c90fd71208c2ee609b314ed183fd6e6cae51e1
Liam Stanley <me@liamstanley.io>
2014-07-07 11:03:02 -0400

    Fix countdown.py

--- modules/countdown.py
+++ modules/countdown.py
@@ -4,15 +4,29 @@ from util.hook import *
 
 @hook(cmds=['countdown'], priority='low', args=True)
 def countdown(code, input):
-    """ .countdown <year> <month> <day> - displays a countdown to a given date. """
-    error = '{red}Please use correct format: %scountdown <year> <month> <day>' % code.prefix
-    text = input.group(2).split()
-    if text[0].isdigit() and text[1].isdigit() and text[2].isdigit() and len(text) == 3:
-        diff = datetime.datetime(
-            int(text[0]), int(text[1]), int(text[2])) - datetime.datetime.today()
-        code.say(
-            str(diff.days) + "-days " + str(diff.seconds / 60 / 60) +
-            "-hours " + str(diff.seconds / 60 - diff.seconds / 60 / 60 * 60) +
-            "-minutes until " + text[0] + " " + text[1] + " " + text[2])
+    """ .countdown <month> <day> <year> - displays a countdown to a given date. """
+    error = '{red}Please use correct format: %scountdown <month> <day> <year>' % code.prefix
+    text = input.group(2).strip()
+    if ' ' in text:
+        text = text.split()
+    elif '/' in text:
+        text = text.split('/')
+    elif '.' in text:
+        text = text.split('.')
     else:
-        code.say(error)
+        return code.say(error)
+    if len(text) != 3:
+        return code.say(error)
+    if not text[0].isdigit() or not text[1].isdigit() or not text[2].isdigit():
+        return code.say(error)
+    month, day, year = text
+    try:
+        diff = datetime.datetime(int(year), int(month), int(day)) - datetime.datetime.today()
+    except ValueError:
+        return code.say('{red}Incorrect input!')
+    output = []
+    output.append(str(diff.days) + " day(s)") 
+    output.append(str(diff.seconds / 60 / 60) + " hour(s)")
+    output.append(str(diff.seconds / 60 - diff.seconds / 60 / 60 * 60) + " minute(s)")
+    output.append(month + "/" + day + "/" + year)
+    code.say(' - '.join(output))
