b54594353a0e5d624c15e54571962fcf7978413b
Liam Stanley <me@liamstanley.io>
2014-07-20 21:24:02 -0400

    Work on version command/github versioning?

---
+++ core/modules/info.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from util.hook import *
+from subprocess import Popen, PIPE
+
+def git_info():
+    p = Popen(["git", "log", "-n 1"], stdout=PIPE)
+    tmp = p.stdout.read().split('\n', 3)
+    new = []
+    for item in tmp:
+        item = item.replace('\n', '')
+        while '  ' in item:
+            item = item.replace('  ', ' ')
+        item = item.strip()
+        new.append(item)
+    commit, author, date, message = new
+    commit = commit.split(' ', 1)[1]
+    author = author.split(' ', 1)[1]
+    date = date.split(' ', 1)[1]
+    return commit, author, date, message
+
+
+@hook(cmds=['version', 'v'], rate=30)
+def version(code, input):
+    """Try to get version (commit) data from git (if installed)"""
+    try:
+        commit, author, date, message = git_info()
+        code.reply('Running version:')
+        code.say(' - {b}Commit{b}: %s ({b}%s{b})' % (commit, message))
+        code.say(' - {b}Author{b}: %s' % author)
+        code.say(' - {b}Date{b}: %s' % date)
+        code.say(' - {b}Source{b}: https://github.com/Liamraystanley/Code/')
+    except:
+        code.say(
+            '%s does not use Github file management. Unable to determine version.' %
+            code.nick)
+
+
+@hook(rule='\x01VERSION\x01', rate=20)
+def ctcp_version(code, input):
+    commit, author, date = git_info()
+    date = date.replace('  ', ' ')
+
+    code.write(
+        ('NOTICE', input.nick), '\x01VERSION {0} : {1}\x01'.format(
+            commit, date)
+    )
+
+
+@hook(rule='\x01SOURCE\x01', rate=20)
+def ctcp_source(code, input):
+    code.write(
+        ('NOTICE',
+         input.nick), '\x01SOURCE https://github.com/Liamraystanley/Code/\x01'
+    )
+    code.write(
+        ('NOTICE', input.nick), '\x01SOURCE\x01'
+    )
+
+
+@hook(rule='\x01PING\s(.*)\x01', rate=10)
+def ctcp_ping(code, input):
+    text = input.group()
+    text = text.replace('PING ', '')
+    text = text.replace('\x01', '')
+    code.write(
+        ('NOTICE', input.nick), '\x01PING {0}\x01'.format(text)
+    )
+
+
+@hook(rule='\x01TIME\x01', rate=20)
+def ctcp_time(code, input):
+    dt = datetime.now()
+    current_time = dt.strftime('%A, %d. %B %Y %I:%M%p')
+    code.write(
+        ('NOTICE', input.nick), '\x01TIME {0}\x01'.format(current_time)
+    )
--- modules/github.py
+++ modules/github.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 from util.hook import *
-from subprocess import *
 from util import web
 
 repo_api = 'https://api.github.com/repos/%s'  # Username/Repo
@@ -79,70 +77,3 @@ def github(code, input):
         output.append('%s %s' % (response['subscribers_count'], u'\u2764'))
         output.append(response['html_url'])
         return code.say(spacer.join(output))
-
-
-def git_info():
-    p = Popen(["git", "log", "-n 1"], stdout=PIPE, close_fds=True)
-
-    commit = p.stdout.readline()
-    author = p.stdout.readline()
-    date = p.stdout.readline()
-    return commit, author, date
-
-
-@hook(cmds=['version', 'v'], rate=30)
-def version(code, input):
-    """Try to get version (commit) data from git (if installed)"""
-    try:
-        commit, author, date = git_info()
-        code.say(str(input.nick) + ": running version:")
-        code.say('  ' + commit)
-        code.say('  ' + author)
-        code.say('  ' + date)
-        code.say('  ' +
-                 '{b}Source:{b} https://github.com/Liamraystanley/Code/')
-    except:
-        code.say(
-            '%s does not use Github file management. Unable to determine version.' %
-            code.nick)
-
-
-@hook(rule='\x01VERSION\x01', rate=20)
-def ctcp_version(code, input):
-    commit, author, date = git_info()
-    date = date.replace("  ", "")
-
-    code.write(
-        ('NOTICE', input.nick), '\x01VERSION {0} : {1}\x01'.format(
-            commit, date)
-    )
-
-
-@hook(rule='\x01SOURCE\x01', rate=20)
-def ctcp_source(code, input):
-    code.write(
-        ('NOTICE',
-         input.nick), '\x01SOURCE https://github.com/Liamraystanley/Code/\x01'
-    )
-    code.write(
-        ('NOTICE', input.nick), '\x01SOURCE\x01'
-    )
-
-
-@hook(rule='\x01PING\s(.*)\x01', rate=10)
-def ctcp_ping(code, input):
-    text = input.group()
-    text = text.replace('PING ', '')
-    text = text.replace('\x01', '')
-    code.write(
-        ('NOTICE', input.nick), '\x01PING {0}\x01'.format(text)
-    )
-
-
-@hook(rule='\x01TIME\x01', rate=20)
-def ctcp_time(code, input):
-    dt = datetime.now()
-    current_time = dt.strftime('%A, %d. %B %Y %I:%M%p')
-    code.write(
-        ('NOTICE', input.nick), '\x01TIME {0}\x01'.format(current_time)
-    )
