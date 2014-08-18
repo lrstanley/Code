c98a515b8a45bac81f177fdb6985fe32580bb894
Liam Stanley <me@liamstanley.io>
2014-08-18 06:24:31 -0400

    Should fix issue #63 (HTML entities rendered incorrectly), and refractor code a bit.

--- modules/lastfm.py
+++ modules/lastfm.py
@@ -2,9 +2,6 @@ import re
 from util import web
 from util.hook import *
 
-# This is the default user to check for last fm
-defaultuser = 'liamraystanley'
-
 
 def getdata(user):
     try:
@@ -14,29 +11,14 @@ def getdata(user):
         return False
     if 'No user exists with this name.' in data:
         return False
-    else:
-        return data
+    return data
 
 
-@hook(cmds=['lastfm', 'lfm'], ex='lastfm liamraystanley', rate=10)
+@hook(cmds=['lastfm', 'lfm'], ex='lastfm liamraystanley', args=True, rate=10)
 def lastfm(code, input):
-    # just .lfm
-    if defaultuser and not input.group(2):
-        user = defaultuser
-        data = getdata(user)
-        if not data:
-            return code.say('Username %s does not exist in the last.fm database.' % (user))
-        else:
-            song = re.compile(r'<title>.*?</title>').findall(data)[1]
-            song = re.sub(r'\<.*?\>', '', song).strip()
-            code.reply('{purple}%s{c} {red}(via Last.Fm)' % song)
-    # .lfm <username>
-    elif input.group(2):
-        user = input.group(2).split()[0].strip().lower()
-        data = getdata(user)
-        if not data:
-            return code.say('Username %s does not exist in the last.fm database.' % (user))
-        else:
-            song = re.compile(r'<title>.*?</title>').findall(data)[1]
-            song = re.sub(r'\<.*?\>', '', song).strip()
-            code.reply('{purple}%s{c} {red}(via Last.Fm)' % song)
+    user = input.group(2).split()[0].strip().lower()
+    data = getdata(user)
+    if not data:
+        return code.say('Username %s does not exist in the last.fm database.' % (user))
+    song = web.striptags(re.compile(r'<title>.*?</title>').findall(data)[1])
+    code.reply('{purple}%s{c} {red}(via Last.Fm)' % web.htmlescape(song))
