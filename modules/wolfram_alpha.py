a3026ac889498054fd1dd182a7f3cf538e2c76a1
Liam Stanley <me@liamstanley.io>
2014-10-03 12:28:33 -0400

    Dat spelling

--- modules/wolfram_alpha.py
+++ modules/wolfram_alpha.py
@@ -3,19 +3,19 @@ from util import web
 
 @hook(cmds=['wa'], ex='wa 1 mile in feet', args=True)
 def wa(code, input):
-    """Wolphram Alpha search"""
+    """Wolfram Alpha search"""
     query = input.group(2)
     uri = 'http://tumbolia.appspot.com/wa/'
     try:
         answer = web.get(uri + web.quote(query), timeout=10).read()
     except timeout:
-        return code.say('It seems WolphramAlpha took too long to respond!')
+        return code.say('It seems WolframAlpha took too long to respond!')
 
     if answer and 'json stringified precioussss' not in answer:
         answer = answer.split(';')
         if len(answer) > 3:
             answer = answer[1]
-        answer = '{purple}{b}WolphramAlpha: {c}{b}' + answer
+        answer = '{purple}{b}WolframAlpha: {c}{b}' + answer
         while '  ' in answer:
             answer = answer.replace('  ', ' ')
         return code.say(web.htmlescape(answer))
