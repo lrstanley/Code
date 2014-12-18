a2b59eadb2cb28a2087375c73fd4262c3de63b45
Liam Stanley <me@liamstanley.io>
2014-12-18 07:56:42 -0500

    Refractoring, update for new web api

--- modules/clock.py
+++ modules/clock.py
@@ -1,30 +1,27 @@
-from util import web
 import time
 import math
+from util import web
 from util.hook import *
 
-uri = 'http://www.timeapi.org/%s/now'
+uri = 'http://www.timeapi.org/{timezone}/now'
 
 
 @hook(cmds=['t', 'time'], ex='t UTC')
 def get_time(code, input):
-    """Returns the current time."""
-    default = 'est'
+    """time <abbreviated timezone> -- Returns the current time. Yucky"""
     fmt = 'Time in {timezone} is {hour}:{minute}:{second} ({month}/{day}/{year})'
-    err = 'Incorrect timezone. Syntax: .time <timezone>'
     if not input.group(2):
-        timezone = default
+        timezone = 'EST'
     elif len(input.group(2).split()) > 1:
-        timezone = default
+        timezone = 'EST'
     else:
-        timezone = input.group(2).lower().strip()
+        timezone = input.group(2)
 
-    # Here, try and get the timezone, using the 'uri'
     try:
-        r = web.get(uri % timezone).read()
-
-        # Try to parse the string data... this will be fun!
-        # Example output from the server... "2014-01-07T09:44:58-05:00"
+        try:
+            r = web.text(uri.format(timezone=timezone.strip().upper()), timeout=15)
+        except:
+            return code.say('Unable to calculate time for that timezone.')
         date, time = r.split('T')
         year, month, day = date.split('-')[::1]
         hour, minute, second = time.split(':', 2)
@@ -37,12 +34,12 @@ def get_time(code, input):
             timezone=timezone.upper()
         ))
     except:
-        return code.say(err)
+        return code.say('Incorrect timezone. Syntax: .time <timezone>')
 
 
 @hook(cmds=['beats'])
 def beats(code, input):
-    """Shows the internet time in Swatch beats."""
+    """beats -- Shows the internet time in Swatch beats."""
     beats = ((time.time() + 3600) % 86400) / 86.4
     beats = int(math.floor(beats))
     code.say('@%03i' % beats)
