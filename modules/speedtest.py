60ab8f1f389aa080d31b8b48793184c125d35748
Liam Stanley <me@liamstanley.io>
2014-12-27 06:05:50 -0500

    Add speedtest.py; Auto-responder for Speedtest.net results

---
+++ modules/speedtest.py
@@ -0,0 +1,27 @@
+from util import web
+from util.hook import *
+
+
+# http://www.speedtest.net/my-result/<id>
+# http://www.speedtest.net/result/<id>.png
+
+uri_regex = r'.*https?://www.speedtest.net/(?:result|my-result)/([0-9]{5,15})(?:\.png)?.*'
+uri = 'http://www.speedtest.net/my-result/%s'
+
+
+@hook(rule=uri_regex, thread=False, supress=True)
+def speedtest(code, input):
+    id = input.group(1)
+    raw = web.clean(web.text(uri % id))
+    raw = raw.split('<div class="share-main">', 1)[1]
+    raw = raw.split('</div><!--/share-main-->', 1)[0]
+    results = web.findin(r'<p>.*?</p>', raw)[:-1]
+    keys = ['Download', 'Upload', 'Ping', 'Carrier', 'Server']
+    tmp = []
+
+    for i in range(0, len(results)-1):
+        if len(web.striptags(results[i])) < 1:
+            continue
+        tmp.append('{b}%s{b}: %s' % (keys[i], web.striptags(results[i])))
+
+    code.say(' {b}-{b} '.join(tmp))
