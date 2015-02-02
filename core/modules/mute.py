42161f1865a9041b9387e02cd0d98375679aa421
Liam Stanley <me@liamstanley.io>
2015-02-02 02:11:45 -0500

    More documentation, and remove duplicate module

--- core/modules/mute.py
+++ core/modules/mute.py
@@ -1,13 +1,15 @@
 from util.hook import *
 
 
-@hook(cmds=['mute'], admin=True, thread=False)
+@hook(cmds=['mute'], trusted=True, thread=False)
 def mute(code, input):
+    """ mute - Mutes the bot. Only usable by admins, owners, and ops. """
     code.mute()
     return code.action('is now muted.')
 
 
-@hook(cmds=['unmute'], admin=True, thread=False)
+@hook(cmds=['unmute'], trusted=True, thread=False)
 def unmute(code, input):
+    """ unmute - Unmutes the bot. Only usable by admins, owners, and ops. """
     code.unmute()
     return code.action('is now unmuted.')
