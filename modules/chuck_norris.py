d7c2bf3a15d4c2600ee244873d335379a8a80b76
Liam Stanley <me@liamstanley.io>
2017-01-13 15:54:13 -0500

    add support for specific chuck norris quotes

--- modules/chuck_norris.py
+++ modules/chuck_norris.py
@@ -6,8 +6,10 @@ from util import web
 def chuck(code, input):
     """Get random Chuck Norris facts. I bet he's better than you."""
     try:
-        data = web.json('http://api.icndb.com/jokes/random')
+        if input.group(2) and input.group(2).isdigit():
+            data = web.json('http://api.icndb.com/jokes/' + input.group(2))
+        else:
+            data = web.json('http://api.icndb.com/jokes/random')
     except:
         return code.say('Chuck seems to be in the way. I\'m not fucking with him.')
-    code.say('#{blue}%s{c} - %s' %
-             (data['value']['id'], web.escape(data['value']['joke'])))
+    code.say('#{blue}%s{c} - %s' % (data['value']['id'], web.escape(data['value']['joke'])))
